6a7e5af483bb01617948e294bcf0ae3e5db1b77d
MaxiProgram <maxiprogram@gmail.com>
2016-10-02 02:02:07 +0300

    Add Level24, Repaint

--- SRC/playprofile.cpp
+++ SRC/playprofile.cpp
@@ -36,7 +36,7 @@ void PlayProfile::ResetScorePlan()
     score_plan[20] = 1;
     score_plan[21] = 32;
     score_plan[22] = 23;
-    score_plan[23] = 1;
+    score_plan[23] = 28;
     score_plan[24] = 1;
     score_plan[25] = 1;
     score_plan[26] = 1;
